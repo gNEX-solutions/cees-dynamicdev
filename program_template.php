9e1c828c0424fd5e4349c80fca9be6ef3d70c136
Rasitha Gamage <g.rasitha@gmail.com>
2019-08-25 19:11:42 +0530

    program template page added

---
+++ program_template.php
@@ -0,0 +1,134 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<header>
+<title>Program</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="assets/services/style.css">
+  <link rel="stylesheet" href="assets/theme/css/preloader.css">
+
+</header>
+
+<body>
+
+<div id="preloader">
+  <div id="status">&nbsp;</div>
+</div>
+
+<section class="menu cid-ruNsw1yRec" once="menu" id="menu1-0">
+    <?php require_once 'common/Components/header.php'; ?>
+</section>
+
+<section class="features1 cid-ruNBDZ0eEF" >
+<h1 class="mbr-title pt-2 mbr-fonts-style display-2" style="text-align:center;">Program name</h1>
+</section>
+
+<section class="features11 cid-ruXpPiazmW" id="features11-19">
+    <div class="container">   
+        <div class="col-md-12">
+            <div class="media-container-row">
+                 <div class="col-md-6" style="overflow:hidden">
+                     <img src="assets/images/person-mountain-top_1160-904.jpg">
+                 </div>
+                 <div class="col-md-6" >
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                 </div>
+            </div>
+            <div class="media-container-row" style="margin-top:40px">
+                <p class="mbr-text mbr-fonts-style display-7">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+
+            <div class="media-container-row">
+                <div>
+                <table>
+                    <tr>
+                        <th>Program code</th>
+                        <td>Maria Anders</td>
+                    </tr>
+                    <tr>
+                        <th>Program Name</th>
+                        <td>Francisco Chang</td>
+                    </tr>
+                    <tr>
+                        <th>Venue</th>
+                        <td>Francisco Chang</td>
+                    </tr>
+                    <tr>
+                        <th>time</th>
+                        <td>4.00PM-6.00PM</td>
+                    </tr>
+                    <tr>
+                        <th>Content</th>
+                        <td>Lorem Ipsum is simply dummy text</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>Lorem Ipsum is simply dummy text</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>Lorem Ipsum is simply dummy text</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>Lorem Ipsum is simply dummy text</td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>Lorem Ipsum is simply dummy text</td>
+                    </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/theme/js/preloader.js"></script>
+
+  <style>
+.cid-ruNBDZ0eEF {
+    padding-top:30px !important;
+}
+.cid-ruXpPiazmW{
+    /* margin:auto !important; */
+}
+</style>
+
+</body>
+
+
+
+</html>
