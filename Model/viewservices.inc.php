8cda886f7f1684794da699c02cc2b70ffb71e7dd
kalpa wijesooriya <kalpa.wijesooriya@yahoo.com>
2019-08-25 16:09:52 +0530

    change variables according to Diniths' commets

--- Model/viewservices.inc.php
+++ Model/viewservices.inc.php
@@ -8,7 +8,7 @@ class ViewServices extends Services {
    { 
        $datas=$this->getAllServices();
        foreach($datas as $data){
-         if($data['type']=='CA_MENU'){
+         if($data['type']=='CA'){
           echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="cesAcadamy.php?idconsultancies='.$data['idconsultancies'].'" >'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
          }
       
@@ -20,7 +20,7 @@ class ViewServices extends Services {
    { 
        $datas=$this->getAllServices();
        foreach($datas as $data){
-         if($data['type']=='CS_MENU'){
+         if($data['type']=='CS'){
           echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="Programs.php">'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
          }
       
@@ -32,7 +32,7 @@ class ViewServices extends Services {
    { 
        $datas=$this->getAllServices();
        foreach($datas as $data){
-         if($data['type']=='SL_MENU'){
+         if($data['type']=='SL'){
           echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="Programs.php" >'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
          }
       
