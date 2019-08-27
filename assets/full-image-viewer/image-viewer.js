b51a9bd0cd0d18b704ef4a839de907733646cec2
Rasitha Gamage <g.rasitha@gmail.com>
2019-08-27 16:57:10 +0530

    image full viewer for event slider is added

---
+++ assets/full-image-viewer/image-viewer.js
@@ -0,0 +1,23 @@
+function viewImageFunction(image_id){
+  var img = document.getElementById(image_id);
+// alert(img.style.backgroundImage.substring(24,img.style.backgroundImage.length-2));
+    // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+  var modalImg = document.getElementById("img01");
+
+  modal.style.display = "block";
+  modalImg.src =img.style.backgroundImage.substring(24,img.style.backgroundImage.length-2);
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+}
+
+
