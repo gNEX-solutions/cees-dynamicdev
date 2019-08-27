846789c8481cd53b9268e22914766a47ddc73b09
Dumindu De Silva <Dumidumadura@gmail.com>
2019-08-27 13:22:39 +0530

    login fogot ps style updated

--- AdminPanel/login.php
+++ AdminPanel/login.php
@@ -33,7 +33,7 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block"><img src="img/cees.png" alt="cees" style="width:50%; margin: 20%;"></div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
@@ -83,7 +83,7 @@
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    <a class="small" href="forgot-password.php">Forgot Password?</a>
                   </div>
                   <!-- <div class="text-center">
                     <a class="small" href="register.html">Create an Account!</a>
