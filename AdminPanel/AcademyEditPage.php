5f2e9a767d237ff6052ae18222dd1aa4ffaed461
dinith72 <jdinith72@gmail.com>
2019-08-27 21:53:33 +0530

    putting the adminEditpage to AdminPannel

--- AdminPanel/AcademyEditPage.php
+++ AdminPanel/AcademyEditPage.php
@@ -10,18 +10,18 @@
   <meta name="author" content="">
 
   <title>Academy Edit Page</title>
-  <?php include '../resources/nav.php'; ?>
-  <?php include '../resources/footer.php'; ?>
+  <?php include './resources/nav.php'; ?>
+  <?php include './resources/footer.php'; ?>
   <!-- including the database connection  -->
-  <?php include '../../Model/dbh.inc.php'; ?>
-  <?php include '../AdminModel/editPage.php'; ?>
+  <?php include '../Model/dbh.inc.php'; ?>
+  <?php include 'AdminModel/editPage.php'; ?>
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Custom styles for this template-->
-  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="./css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -273,7 +273,7 @@ $conn=$newConnection->connect(); ?>
               
               echo("
               <input type=\"hidden\" name=\"inputId\" value=" . $row['idconsultancies_images']. ">
-                <img src=../../". $row['url']." alt='..' class='img-thumbnail' style=' max-height: 150px'>
+                <img src=../". $row['url']." alt='..' class='img-thumbnail' style=' max-height: 150px'>
                 <button type=\"submit\" name=\"img_remove\" class=\"btn btn-outline-danger\" > Remove </button>
               
               ");
