066e91d79a921ba41f72f3376db3726bfed28f14
ANGEL-141 <umranawaka@gmail.com>
2019-08-28 23:02:27 +0530

    Dymanic Consulting Post loading

---
+++ Model/consultService.php
@@ -0,0 +1,60 @@
+<?php
+//include 'dbh.inc.php';
+
+class consultService extends consultWindow
+{
+
+    public function ShowConsult_List()
+    {
+        $datas = $this->getAllConsultations();
+        foreach ($datas as $data) {
+
+            $url = explode("/", $data['url']);
+
+            if ($data['position'] == "LU") {
+
+                echo '<div class="grid-flex hideme">
+                    <div class="col col-image">
+                    <img src="assets/images/consultancy/' . $url[2] . ' " alt="" class="mx-auto d-block"  style="max-height: 450px; max-width: 450px;"/>
+                    </div>
+                    <div class="col col-text">
+                    <div class="Aligner-item text-left">
+                        <h3 class="text-left display-2 font-weight-normal">' . $data['heading'] . '</h3>
+                        <p class="text-justify">' . $data['summary'] . '</p>
+                    </div>
+                    </div>
+                    </div>
+            
+                    <hr>';
+            } elseif ($data['position'] == "LD") {
+                echo '<div class="grid-flex hideme">
+                    <div class="col col-image">
+                    <img src="assets/images/consultancy/' . $url[2] . ' " alt="" class="mx-auto d-block" style="max-height: 450px; max-width: 450px;" />
+                    </div>
+                    <div class="col col-text col-left" style="margin-left:75px;">
+                    <div class="Aligner-item">
+                        <h3 class="text-left display-2 font-weight-normal">' . $data['heading'] . '</h3>
+                        <p class="text-justify">' . $data['summary'] . '</p>
+                    </div>
+                    </div>
+                    </div>
+
+                    <hr>';
+            } else {
+                echo '<div class="grid-flex hideme">
+                <div class="col col-image">
+                <img src="assets/images/consultancy/' . $url[2] . ' " alt="" class="mx-auto d-block" style="max-height: 450px; max-width: 450px;" />
+                </div>
+                <div class="col col-text">
+                <div class="Aligner-item text-left">
+                    <h3 class="text-left display-2 font-weight-normal">' . $data['heading'] . '</h3>
+                    <p class="text-justify">' . $data['summary'] . '</p>
+                </div>
+                </div>
+                </div>
+        
+                <hr>';
+            }
+        }
+    }
+}
