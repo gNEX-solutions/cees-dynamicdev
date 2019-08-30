7b1e45a8bac6d50512f3639f4e358a4daffdf08e
Dumindu De Silva <Dumidumadura@gmail.com>
2019-08-30 14:26:33 +0530

    consultency page styling done

--- AdminPanel/index.php
+++ AdminPanel/index.php
@@ -9,6 +9,8 @@
 <?php include 'resources/nav.php'; ?>
   <?php include 'resources/footer.php'; ?>
 <head>
+
+<link rel="shortcut icon" href="../assets/images/logo2.png" type="image/x-icon">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
