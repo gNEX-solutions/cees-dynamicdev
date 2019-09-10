ac87b2c2e07e64eccb53e81f825c8cda2c79763b
DharshithaSrimal <dharshithasrimal@gmail.com>
2019-09-10 20:12:47 +0530

    Create sections

---
+++ Model/viewSections.inc.php
@@ -0,0 +1,57 @@
+  
+<?php
+
+class ViewSections extends Services {
+    
+ 
+  public function ShowCA_MENU()
+   { 
+       $datas=$this->getAllServices();
+       foreach($datas as $data){
+         if($data['type']=='CA'){
+          echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="program_template.php?idconsultancies='.$data['idconsultancies'].'&heading='.$data['heading'].'" >'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
+         }
+      
+
+      }
+      
+   }
+   public function ShowCS_MENU()
+   { 
+       $datas=$this->getAllServices();
+       foreach($datas as $data){
+         if($data['type']=='CS'){
+          echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="program_template.php?idconsultancies='.$data['idconsultancies'].'&heading='.$data['heading'].'">'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
+         }
+      
+
+      }
+      
+   }
+   public function ShowSL_MENU()
+   { 
+       $datas=$this->getAllServices();
+       foreach($datas as $data){
+         if($data['type']=='SL'){
+          echo '<div class="col-md-3"><h4 class="dropdown-heading"><a  href="program_template.php?idconsultancies='.$data['idconsultancies'].'&heading='.$data['heading'].'" >'. $data['heading'].'</a></h4><ul class="dropdown-list"><li>'. $data['summary'].' </li></ul></div>';
+         }
+      
+
+      }
+      
+   }
+   public function ShowCA_CONTENT($idconsultancies)
+   { 
+       $datas=$this->  getRequestedServiceData ($idconsultancies);
+       return  $datas;  
+   }
+   public function showCA_IMAGES($idconsultancies)
+   {
+      $datas=$this->  getRequestedServiceImages ($idconsultancies);
+      return  $datas;  
+   }
+
+ 
+}
+?>
+
