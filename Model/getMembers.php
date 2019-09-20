3203e7282770596ab6451056e3244bbadc31248e
Benuka <benuka02@gmail.com>
2019-09-20 23:35:16 +0530

    SL view correction

---
+++ Model/getMembers.php
@@ -0,0 +1,21 @@
+<?php
+
+class getMembers extends dbh{
+    
+   public function getAllMembers()
+   { 
+      $sql="SELECT idTeam_members,first_name,last_name,role FROM team_members";
+      $result=$this->connect()->query($sql);
+      $numRows=$result->num_rows;
+      if($numRows>0){
+          while($row=$result->fetch_assoc()){
+             $data[]=$row;
+          }
+          return $data ;
+      }
+    }
+
+
+  }
+
+?>
