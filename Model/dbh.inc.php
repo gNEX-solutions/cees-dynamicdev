e5a77f717f778d7d710e38f48eedf599c679f204
dinith72 <jdinith72@gmail.com>
2019-10-04 21:32:11 +0530

    changing the location of the server to the localhost

--- Model/dbh.inc.php
+++ Model/dbh.inc.php
@@ -12,7 +12,10 @@ class dbh{
     //    $this->userName="b2549a7be081f5";
     //    $this->password="9777311d";
     //    $this->dbName="heroku_3dffaa1b8ca65ff";
-    $this->serverName="138.128.162.194";
+
+ //   changing the db server from server addres to the localhost
+   //  $this->serverName="138.128.162.194";  commented by : dj
+      $this->serverName="127.0.0.1";
        $this->userName="ceesinte_developer";
        $this->password="1v_UJ8]QYtoL";
        $this->dbName="ceesinte_demo";
