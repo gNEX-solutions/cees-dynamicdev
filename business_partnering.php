a0bf30329db42c7b3b79ba64f1878b5a371bfd27
Pasan Peiris <pasan.peiris01@gmail.com>
2020-09-06 15:38:36 +0530

    hide boxes if images not set

--- business_partnering.php
+++ business_partnering.php
@@ -65,14 +65,14 @@ $artID =  $_GET['artID'];
                 </div>
                 <div class="col-lg-4 right-contents">
                     <div class="container">
-                        <div class="container right-contents" style="padding-bottom: 20px;  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);">
+                       
                         <?php $businessPartneringView->ShowImage($artID);  ?>
-                        </div>
+                        
                     </div>
                     <div class="container">
-                        <div class="container right-contents" style="padding-bottom: 20px;  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);">
+                        
                         <?php $businessPartneringView->ShowImage2($artID);  ?>
-                        </div>
+                       
                     </div>
 
 
